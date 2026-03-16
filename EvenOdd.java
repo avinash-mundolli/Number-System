8fd4e2e581411b5e1fe78b01c586d5ca617bd61f
Avinash Mundolli <avinashmundolli@gmail.com>
2026-03-16 11:38:49 +0530

    EvenOdd

---
+++ EvenOdd.java
@@ -0,0 +1,10 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num=13;
+        if(num%2==0){
+            System.out.println(num+" even number");
+        }else{
+            System.out.println(num+" is odd number");
+        }
+    }
+}
